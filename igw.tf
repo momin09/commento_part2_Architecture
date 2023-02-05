f3162d042b62e2c77b99380df65e54f1f237c1d1
momin09 <khm0909z@gmail.com>
2023-02-06 01:14:54 +0900

    Add: IGW and Routing Table for VPC

--- igw.tf
+++ igw.tf
@@ -7,29 +7,64 @@ resource "aws_internet_gateway" "commento-igw" {
   vpc_id = aws_vpc.commento-vpc.id 
 }
 
-resource "aws_route_table" "commento-route-table" {
+resource "aws_route_table" "commento-route-table-public" {
   tags = {
     Name = "RT-AN2-DAI-PRD-PUB"
     Description = "Route Table for public subnet"
   }
 
-  route {
+  vpc_id = aws_vpc.commento-vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.commento-igw.id
   }
 }
 
 resource "aws_route_table_association" "commento-route-table-association-pub01" {
   subnet_id = aws_subnet.commento-sub-pub01.id
-  route_table_id = aws_route_table.commento-route-table.id
+  route_table_id = aws_route_table.commento-route-table-public.id
 }
 
 resource "aws_route_table_association" "commento-route-table-association-pub02" {
   subnet_id = aws_subnet.commento-sub-pub02.id
-  route_table_id = aws_route_table.commento-route-table.id
+  route_table_id = aws_route_table.commento-route-table-public.id
 }
 
-resource "aws_route_table" "commento-route-table" {
-    tags = {
-        Name = ""
-    }
+resource "aws_route_table" "commento-route-table-priv01" {
+  tags = {
+    Name = "RT-AN2-DAI-PRD-PRIV1"
+    Description = "Route Table for private subnet1"
+  }
+
+  vpc_id = aws_vpc.commento-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.commento-igw.id
+  }
+}
+
+resource "aws_route_table" "commento-route-table-priv02" {
+  tags = {
+    Name = "RT-AN2-DAI-PRD-PRIV2"
+    Description = "Route Table for private subnet2"
+  }
+
+  vpc_id = aws_vpc.commento-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.commento-igw.id
+  }
+}
+
+resource "aws_route_table_association" "commento-route-table-association-priv01" {
+  subnet_id = aws_subnet.commento-sub-priv01.id
+  route_table_id = aws_route_table.commento-route-table-priv01.id
+}
+
+resource "aws_route_table_association" "commento-route-table-association-priv02" {
+  subnet_id = aws_subnet.commento-sub-priv02.id
+  route_table_id = aws_route_table.commento-route-table-priv02.id
 }
